0cbccadb22ee871d968d1229d5d9b1e23e3ff562
Your Name <you@example.com>
2025-02-11 11:53:54 +0800

    <update> clean up default pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>This is home page</h1>
   </main>
 </template>
