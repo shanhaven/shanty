3a35d446b8421fc2a7c2842cd2d0513502ed9073
Your Name <you@example.com>
2025-02-11 16:41:24 +0800

    <add> vueuse library for dark mode toggling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import {useDark, useToggle} from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <main>
+  <main class="bg-white dark:bg-gray-800">
     <h1>This is home page</h1>
+
+    <p>isDark: {{isDark}}</p>
+    <button class="px-4 py-2 text-white bg-teal-400 rounded-full" @click="toggleDark()" type="button">
+      Toggle Dark Mode Here
+    </button>
   </main>
 </template>
