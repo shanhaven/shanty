637dcc43d81e25d57e5661fab1ce9dc8b527cc0a
Your Name <you@example.com>
2025-02-16 01:55:29 +0800

    <update> pdf file name

--- src/components/me/HeroSection.vue
+++ src/components/me/HeroSection.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
@@ -11,7 +10,7 @@
         <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Front-End Engineer from Parañaque, Philippines, specializing in Vue.js.</p>
         <p class="text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"> I create fast, scalable, and accessible web applications with seamless API integration, emphasizing clean code, high performance, usability, and a minimalist, user-friendly design.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a href="/public/Shanty%20Cajulao.pdf" download="Shanty Cajulao.pdf"
+          <a href="/public/shantycajulao.pdf" download="Shanty Cajulao.pdf"
              class="rounded-md bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-teal-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600">
             Resume <span aria-hidden="true">↓</span>
           </a>
