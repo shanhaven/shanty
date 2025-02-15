9cee528dba5440587456307dbf633f71cfa6d911
Your Name <you@example.com>
2025-02-16 01:29:59 +0800

    <update> isolate hero section on its separate component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,7 @@
 <script setup>
 
+import HeroSection from '@/components/me/HeroSection.vue'
 </script>
 <template>
-    <div class="relative isolate px-6 pt-14 lg:px-8">
-      <div class="mx-auto max-w-2xl py-32">
-        <div class="text-center">
-          <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 dark:text-gray-100 sm:text-7xl">👋 Hey there!</h1>
-          <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 dark:text-gray-100 sm:text-7xl">I'm Shanty Cajulao</h1>
-          <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Front-End Engineer from Parañaque, Philippines, specializing in Vue.js.</p>
-          <p class="text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"> I create fast, scalable, and accessible web applications with seamless API integration, emphasizing clean code, high performance, usability, and a minimalist, user-friendly design.</p>
-          <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a href="#"
-               class="rounded-md bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-teal-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600">
-              Resume <span aria-hidden="true">↓</span>
-            </a>
-            <a href="mailto:shantycajulao@gmail.com"
-               class="rounded-md outline px-3.5 py-2.5 text-sm font-semibold text-teal-600 shadow-xs hover:bg-teal-500 hover:outline-transparent hover:text-white">
-              Email <span aria-hidden="true">✉</span>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+  <HeroSection />
 </template>
